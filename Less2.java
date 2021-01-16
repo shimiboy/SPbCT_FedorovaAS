95dc01ef70e4c001cb8d018e25ebbbd867642261
shimiboy <77536319+shimiboy@users.noreply.github.com>
2021-01-16 16:18:57 +0300

    Create Less2.java

---
+++ Less2.java
@@ -0,0 +1,8 @@
+public class Less2 {
+        public static void main(String[] args) {
+            double a = 3;
+            double b = 4;
+            double c = Math.sqrt (a * a + b * b);
+            System.out.println ("c = "+ c);
+        }
+    }
