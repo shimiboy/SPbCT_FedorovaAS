dcc5a6534ece79bb13e8740094517b78f9ece136
shimiboy <77536319+shimiboy@users.noreply.github.com>
2021-02-03 03:01:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
-public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello Java!");
+package PR3;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Введите число:");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num < 100){
+            System.out.println("less");
+        } else {
+            System.out.println("not less");
         }
     }
+}
