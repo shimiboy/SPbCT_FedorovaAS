dd370e15b3c2789f5f39ec824c0d0c60e91e9f86
shimiboy <77536319+shimiboy@users.noreply.github.com>
2021-04-14 18:09:16 +0300

    Create Task_1.java

---
+++ KR_1/Task_1.java
@@ -0,0 +1,18 @@
+package KR;
+
+public class Task_1 {
+    /*1.	Вывести на экран текст "Silence is golden".*/
+    public static void main(String[] args) {
+        System.out.print("Silence is golden" + "\n");
+
+    }
+}
+class Task_1_2{
+    /*2.	Вывести на экран букву "W" из символов "*" (звездочка).*/
+    public static void main(String[] args) {
+        System.out.println("*     *      *");
+        System.out.println(" *   * *    * ");
+        System.out.println("  * *   * *  ");
+        System.out.println("   *     *   ");
+    }
+}
